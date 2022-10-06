4ddc6df753be55cafdc689d76ee3e0acec5a9286
asthaketwas <asthaketwas@gmail.com>
2022-10-06 21:30:18 +0530

    v1

---
+++ scripts/task.js
@@ -0,0 +1,82 @@
+
+//1st task:- Find the grater value
+let a=10
+let b=20
+let c=30
+if(a>b)
+{
+    console.log("A is grater than B :" + a)
+}
+else if(a>c)
+{
+    console.log("A is grater than C :" + a)
+
+}
+else if(b>c)
+{
+    console.log("B is grater than C :" + b)
+
+}
+else
+{
+console.log("C is grater than A and B :" + c)
+}
+
+//2nd task:- Show the value of admin using console.log.
+
+let admin
+let fname = 'John'
+admin = fname
+console.log( "The value of Admin is:" + admin)
+
+/*3rd task:- Write a program, using the variable declarations above, 
+             that displays the data type for each variable.*/
+    
+let x=100
+let name ='crystal'
+let isArrears=true
+let isTopper=false
+let s=[11,55,2,78,4]
+let d=null
+let k
+console.log( typeof 100)
+console.log( typeof 'crystal')
+console.log( typeof true)
+console.log( typeof false)
+console.log( typeof [11,55,2,78,4])
+console.log( typeof d)
+console.log( typeof k)
+
+//4th task:-What will be the result for these expressions?
+
+console.log( 5 > 4)
+console.log("apple" > "pineapple")
+console.log("2" > "12")
+console.log(undefined == null)
+console.log(undefined === null)
+
+
+let depart ="esc"
+ if(depart==="esc")
+ {
+    console.log("go to first floor")
+ }
+else if(depart==="csd")
+{
+    console.log("go to thrid floor")
+}
+else if(depart==="mtec")
+{
+    console.log("go to second floor")
+}
+else{
+    console.log("wrong input")
+}
+
+for(let i=1; ; i++)
+{
+    console.log(i)
+}
+
+
+     
